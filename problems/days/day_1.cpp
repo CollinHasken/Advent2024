c9ebee26e406143d7916d979aaead2edcf225d9d
Collin Hasken <hasken.collin@gmail.com>
2024-12-03 00:19:35 -0500

    Day 1

--- problems/days/day_1.cpp
+++ problems/days/day_1.cpp
@@ -2,6 +2,11 @@
 
 #include "../common_includes.h"
 
+#include <algorithm>
+#include <map>
+#include <sstream>
+#include <vector>
+
 PROBLEM_CLASS_CPP(1);
 
 void problem_1::solve(const std::string& file_name)
@@ -12,17 +17,38 @@ void problem_1::solve(const std::string& file_name)
 		return;
 	}
 
-	while (!input.eof()) {
-		std::string input_line;
-		std::getline(input, input_line);
+	size_t numLines = 0;
+	std::string input_line;
+	while (std::getline(input, input_line))
+		++numLines;
+	input.clear();
+	input.seekg(0);
 
+	std::vector<int> leftList(numLines), rightList(numLines);
+
+	auto leftItr = leftList.begin();
+	auto rightItr = rightList.begin();
+	while (std::getline(input, input_line))
+	{
+		std::stringstream ss(input_line);
+		ss >> *leftItr >> *rightItr;
+		leftItr++;
+		rightItr++;
 	}
-
 	input.close();
 
-	/*std::string answer;
-	answer = std::to_string(highest_id);
-	output_answer(answer); */
+	std::sort(leftList.begin(), leftList.end());
+	std::sort(rightList.begin(), rightList.end());
+
+	unsigned long long sum = 0;
+	for (auto leftItr = leftList.begin(), rightItr = rightList.begin(); leftItr != leftList.end(); ++leftItr, ++rightItr)
+	{
+		sum += abs(*leftItr - *rightItr);
+	}
+
+	std::string answer;
+	answer = std::to_string(sum);
+	output_answer(answer);
 }
 
 void problem_2::solve(const std::string& file_name)
@@ -33,15 +59,52 @@ void problem_2::solve(const std::string& file_name)
 		return;
 	}
 
-	while (!input.eof()) {
-		std::string input_line;
-		std::getline(input, input_line);
+	size_t numLines = 0;
+	std::string input_line;
+	while (std::getline(input, input_line))
+		++numLines;
+	input.clear();
+	input.seekg(0);
 
+	std::vector<int> leftList(numLines);
+	std::map<int, int> rightOccurances;
+
+	auto leftItr = leftList.begin();
+	while (std::getline(input, input_line))
+	{
+		std::stringstream ss(input_line);
+		ss >> *leftItr;
+		leftItr++;
+		int rightNum = 0;
+		ss >> rightNum;
+		if (auto rightItr = rightOccurances.find(rightNum); rightItr != rightOccurances.end())
+			++rightItr->second;
+		else
+			rightOccurances.emplace(rightNum, 1);
 	}
-
 	input.close();
 
-	/*std::string answer;
-	answer = std::to_string(highest_id);
-	output_answer(answer);*/
+	std::sort(leftList.begin(), leftList.end());
+
+	int lastOccurances = 0;
+	int lastNum = -1;
+	unsigned long long similarity = 0;
+	for (auto leftItr = leftList.begin(); leftItr != leftList.end(); ++leftItr)
+	{
+		if (lastNum == *leftItr)
+		{
+			similarity += lastOccurances;
+			continue;
+		}
+
+		auto rightItr = rightOccurances.find(*leftItr);
+		int occurances = rightItr != rightOccurances.end() ? rightItr->first * rightItr->second : 0;
+		similarity += occurances;
+		lastOccurances = occurances;
+		lastNum = *leftItr;
+	}
+
+	std::string answer;
+	answer = std::to_string(similarity);
+	output_answer(answer);
 }
